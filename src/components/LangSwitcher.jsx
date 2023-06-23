4bfbd87660cf1b0ee01450f744890bce3bc07315
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2023-06-24 00:40:40 +0500

    origin

--- src/components/LangSwitcher.jsx
+++ src/components/LangSwitcher.jsx
@@ -1,13 +1,16 @@
 'use client'
 import Link from 'next/link';
-import {usePathname} from 'next/navigation'
+import {usePathname, useSearchParams} from 'next/navigation'
 const LangSwitcher = ({arabic}) => {
     const pathname = usePathname();
     console.log(pathname)
     let newPath =pathname.includes('/ar') ? pathname.replace('/ar',""): "/ar" +pathname
-    
+    const searchParams = useSearchParams()
+    const page = searchParams.get('page')
+    newPath = page ? newPath + "?page="+page :newPath
   return (
       <Link href={ newPath || "/" }>{arabic ? "En" : "Ar"}</Link> 
+      
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,8 +11,7 @@ export default async function Nav({arabic}) {
  
    
   return (
-    <nav className='flex  justify-between   flex-wrap  p-2 px-4' >
-      
+    <nav className='flex  justify-between   flex-wrap  p-2 px-4' > 
       <div className='flex flex-row-reverse gap-x-3'>
         {data.nav.tree.map(({page})=> <Link key={page.url} href={page.url}><strong>{page.title}</strong></Link>  )}
       </div> 
