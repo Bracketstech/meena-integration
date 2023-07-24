9439e1dced1c8bea1bbc93844541ee3d443e99ea
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2023-07-24 16:00:56 +0500

    origin

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -7,54 +7,71 @@ import TermsAndConditions from "@/components/Pages/TermsAndConditions/Index";
 import PrivacyPolicy from "@/components/Pages/PrivacyPolicy/Index";
 import usePageId from "@/hooks/usePageId";
 import getData from "@/lib/data-hooks/getData";
+import getServicesData from "@/lib/data-hooks/getServicesData";
+import getLocationData from "@/lib/data-hooks/getLocationData";
+import getHelpAndSupportData from "@/lib/data-hooks/getHelpAndSupportData";
+import getAboutData from "@/lib/data-hooks/getAboutData";
+import getCareerData from "@/lib/data-hooks/getCareerData";
+import getTermsData from "@/lib/data-hooks/getTermsData";
+import getPrivacyData from "@/lib/data-hooks/getPrivacyData";
 
 export const revalidate = 5;
 
 export default async function Page({ params }) {
   const { slug } = params;
   const id = await usePageId(slug);
-  const data = await getData(id);
   switch (id) {
     case "300942ec-7d92-4f42-b8b2-15250cbf1c04":
+      const servicesData = await getServicesData(id);
       return (
         <>
-          <Services data={data?.entry} />
+          <Services data={servicesData?.entry} />
         </>
       );
     case "b797d4f3-9da1-48c2-9b37-89250fd85a84":
+      const locationsData = await getLocationData(id);
+
       return (
         <>
-          <Locations data={data?.branches?.data} />
+          <Locations data={locationsData?.branches?.data} />
         </>
       );
     case "ab9bc9b1-cd72-42b7-b086-14ea9e1489ff":
+      const helpAndSupportData = await getHelpAndSupportData(id);
+
       return (
         <>
-          <HelpAndSupport data={data} />
+          <HelpAndSupport data={helpAndSupportData} />
         </>
       );
     case "464bc255-fcaa-40f3-a9b3-6dff74e5436b":
+      const aboutData = await getAboutData(id);
       return (
         <>
-          <AboutMeena data={data?.entry} />
+          <AboutMeena data={aboutData?.entry} />
         </>
       );
     case "2f433ac6-6b0b-4a65-bcb8-2f84b7e6b211":
+      const careersData = await getCareerData(id);
+
       return (
         <>
-          <Careers data={data?.entry} />
+          <Careers data={careersData?.entry} />
         </>
       );
     case "7e1edd52-c907-43c4-8076-36dada4fc25c":
+      const termsData = await getTermsData(id);
+
       return (
         <>
-          <TermsAndConditions data={data?.entry} />
+          <TermsAndConditions data={termsData?.entry} />
         </>
       );
     case "cd61ee96-9117-4b4e-9663-40764ac4ee6f":
+      const privacyData = await getPrivacyData(id);
       return (
         <>
-          <PrivacyPolicy data={data?.entry} />
+          <PrivacyPolicy data={privacyData?.entry} />
         </>
       );
 
