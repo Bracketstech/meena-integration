87c73a20e7263cbd273dcc7c943e1a0287001711
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2023-07-26 20:51:10 +0500

    origin

--- src/components/Pages/HelpAndSupport/components/Form.jsx
+++ src/components/Pages/HelpAndSupport/components/Form.jsx
@@ -163,7 +163,7 @@ const Form = ({ arabic, title, formContent }) => {
         </div>
         <div className="mt-[10px] sm:mt-[1vw] ">
           <ReCAPTCHA
-            sitekey="6LdUlVcnAAAAAMcetons6Hxa93bckjZZzE0jnt0-"
+            sitekey="6Les-1QnAAAAAGg82FEaIT2MjE4Di0_oi_McPYd9"
             onChange={handleRecaptchaChange}
           />
         </div>
