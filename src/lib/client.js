d74311427d0f8194bfb237a0f23c7ef84bba0984
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2023-07-30 19:45:00 +0500

    origin

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,4 +1,9 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+
+import {
+  NextSSRInMemoryCache,
+  NextSSRApolloClient,
+} from "@apollo/experimental-nextjs-app-support/ssr";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
 const cache = new InvalidationPolicyCache({
@@ -9,9 +14,15 @@ const cache = new InvalidationPolicyCache({
 });
 
 export const { getClient } = registerApolloClient(() => {
-  return new ApolloClient({
-    // cache: new InMemoryCache(),
-    cache: cache,
+  // return new ApolloClient({
+  //   // cache: new InMemoryCache(),
+  //   cache: cache,
+  //   link: new HttpLink({
+  //     uri: "https://meenacms.brackets-tech.com/graphql",
+  //   }),
+  // });
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
     link: new HttpLink({
       uri: "https://meenacms.brackets-tech.com/graphql",
     }),
