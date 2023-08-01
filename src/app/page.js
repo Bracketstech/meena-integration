737bba292f3c6e9088a3e4fbe7bdb9053ae40160
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2023-08-01 11:29:48 +0500

    origin

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import getFABData from "@/lib/data-hooks/getFABData";
 import getHomeData from "@/lib/data-hooks/getHomeData";
 import { Suspense } from "react";
 import Loading from "./loading";
+import getGlobalMetaData from "@/lib/data-hooks/getGlobalMetaData";
 export const revalidate = 300;
 
 export async function generateMetadata({ params, searchParams }, parent) {
