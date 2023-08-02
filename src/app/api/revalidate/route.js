15faceafdedc60f4e2b533a829d0b96692091ac8
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2023-08-03 02:38:21 +0500

    origin

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,8 +1,13 @@
-// import { NextResponse } from "next/server";
-// import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
 
-// export async function GET(request) {
-//   const path = request.nextUrl.searchParams.get("path");
-//   revalidatePath(path);
-//   return NextResponse.json({ revalidated: true, now: Date.now() });
-// }
+export async function GET(request) {
+  if (
+    request.nextUrl.searchParams.get("secret") !== process.env.MY_SECRET_TOKEN
+  ) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+  const path = request.nextUrl.searchParams.get("path");
+  revalidatePath(path);
+  return NextResponse.json({ revalidated: true, now: Date.now() });
+}
