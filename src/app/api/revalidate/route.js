610ae154b2eaabc7bc4301e52fa12b3f31b96558
Ahmed <osman.ahmeda91@gmail.com>
2023-08-27 21:27:00 +0300

    changes

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
+
 export async function GET(request) {
   if (
     request.nextUrl.searchParams.get("secret") !== process.env.MY_SECRET_TOKEN
