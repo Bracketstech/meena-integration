9cb0ae5bc413cb808f4c914111fcafb10c3f05be
Ahmed <osman.ahmeda91@gmail.com>
2023-08-30 20:27:23 +0300

    changes

--- src/components/Pages/Home/components/CompanyValue.jsx
+++ src/components/Pages/Home/components/CompanyValue.jsx
@@ -9,7 +9,7 @@ const CompanyValue = ({ index, companyValue }) => {
       className="h-[190px] sm:h-[160px] sm:pt-[6.09756097561vw] sm:w-[100%]  lg:pb-[2.34375vw] companyValues-start w-full lg:pt-[1.74479166667vw] pb-[24px] pt-[8.20512820513vw] lg:h-[11.1979166667vw] lg:w-[23.3333333333vw] bg-[#F5EBE8] lg:rounded-[1.04166666667vw] rounded-[3.84615384615vw] flex lg:items-stretch"
     >
       <div className=" sm:h-full sm:w-[90%] sm:gap-y-[1.46341463415vw] w-[76.6666666667vw] gap-y-[2vw] lg:w-[19.1666666667vw] h-full justify-between mx-[auto] flex flex-col lg:gap-y-[0.41666666666vw]">
-        <h4 className="sm:text-[2.68292682927vw] sm:leading-[3.90243902439vw] text-[4.871794871794872vw] lg:text-[1.354166666666667vw] lg:leading-[1.875vw] PingAR-Bold text-[#845FFF] flex-grow-[1]">
+        <h4 className="lg:min-h-[4.3vw] sm:text-[2.68292682927vw] sm:leading-[3.90243902439vw] text-[4.871794871794872vw] lg:text-[1.354166666666667vw] lg:leading-[1.875vw] PingAR-Bold text-[#845FFF] flex-grow-[1]">
           {companyValue.value_title}
         </h4>
         <p className="sm:text-[2.19512195122vw] sm:leading-[3.65853658537vw] lg:text-[0.9375vw] line_clamp3g lg:leading-[1.45833333333vw] PingAR-Regular text-[#505050] ">
--- src/components/Pages/Home/components/HeroLines.jsx
+++ src/components/Pages/Home/components/HeroLines.jsx
@@ -2,59 +2,60 @@ import React from "react";
 
 const HeroLines = () => {
   return (
-    <div>
-      <svg
-        id="heroLine1"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute hidden lg:block opacity-0 flipped heroLine w-[34.84375vw] leftRightFixer1 bottom-[7.33906249999vw]"
-        viewBox="0 0 675.665 74.816"
-      >
-        <path
-          id="Path_101511"
-          data-name="Path 101511"
-          d="M-3806,711.985s281.7-67.628,669.043-68.194"
-          transform="translate(3809.617 -640.787)"
-          fill="none"
-          stroke="#8450ff"
-          strokeLinecap="round"
-          strokeWidth={6}
-        />
-      </svg>
-      <svg
-        id="heroLine2"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute hidden lg:block opacity-0 flipped heroLine w-[10.15625vw] bottom-[9.871875vw]"
-        viewBox="0 0 201.364 35.191"
-      >
-        <path
-          id="Path_101512"
-          data-name="Path 101512"
-          d="M-2690.248,663.356c72.318-2.322,136.629-12.789,194.592-28.419"
-          transform="translate(2693.343 -631.26)"
-          fill="none"
-          stroke="#8450ff"
-          strokeLinecap="round"
-          strokeWidth={6}
-        />
-      </svg>
-      <svg
-        id="heroLine3"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute hidden lg:block opacity-0 flipped heroLine leftRightFixer2 w-[7.55208333333vw] bottom-[23.3265625vw]"
-        viewBox="0 0 152.08 30.043"
-      >
-        <path
-          id="Path_101510"
-          data-name="Path 101510"
-          d="M-2039.476,405.022c47.375-19.92,94.079-29.007,144.63-19.086"
-          transform="translate(2043.404 -378.907)"
-          fill="none"
-          stroke="#8450ff"
-          strokeLinecap="round"
-          strokeWidth={6}
-        />
-      </svg>
-    </div>
+    <div></div>
+    // <div>
+    //   <svg
+    //     id="heroLine1"
+    //     xmlns="http://www.w3.org/2000/svg"
+    //     className="absolute hidden lg:block opacity-0 flipped heroLine w-[34.84375vw] leftRightFixer1 bottom-[7.33906249999vw]"
+    //     viewBox="0 0 675.665 74.816"
+    //   >
+    //     <path
+    //       id="Path_101511"
+    //       data-name="Path 101511"
+    //       d="M-3806,711.985s281.7-67.628,669.043-68.194"
+    //       transform="translate(3809.617 -640.787)"
+    //       fill="none"
+    //       stroke="#8450ff"
+    //       strokeLinecap="round"
+    //       strokeWidth={6}
+    //     />
+    //   </svg>
+    //   <svg
+    //     id="heroLine2"
+    //     xmlns="http://www.w3.org/2000/svg"
+    //     className="absolute hidden lg:block opacity-0 flipped heroLine w-[10.15625vw] bottom-[9.871875vw]"
+    //     viewBox="0 0 201.364 35.191"
+    //   >
+    //     <path
+    //       id="Path_101512"
+    //       data-name="Path 101512"
+    //       d="M-2690.248,663.356c72.318-2.322,136.629-12.789,194.592-28.419"
+    //       transform="translate(2693.343 -631.26)"
+    //       fill="none"
+    //       stroke="#8450ff"
+    //       strokeLinecap="round"
+    //       strokeWidth={6}
+    //     />
+    //   </svg>
+    //   <svg
+    //     id="heroLine3"
+    //     xmlns="http://www.w3.org/2000/svg"
+    //     className="absolute hidden lg:block opacity-0 flipped heroLine leftRightFixer2 w-[7.55208333333vw] bottom-[23.3265625vw]"
+    //     viewBox="0 0 152.08 30.043"
+    //   >
+    //     <path
+    //       id="Path_101510"
+    //       data-name="Path 101510"
+    //       d="M-2039.476,405.022c47.375-19.92,94.079-29.007,144.63-19.086"
+    //       transform="translate(2043.404 -378.907)"
+    //       fill="none"
+    //       stroke="#8450ff"
+    //       strokeLinecap="round"
+    //       strokeWidth={6}
+    //     />
+    //   </svg>
+    // </div>
   );
 };
 
