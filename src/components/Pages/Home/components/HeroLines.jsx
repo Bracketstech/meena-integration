5db83892e4d855b4a81b6eeadf3d2b97318ca965
Ahmed <osman.ahmeda91@gmail.com>
2023-08-30 20:34:06 +0300

    ch

--- src/components/Pages/Home/components/HeroLines.jsx
+++ src/components/Pages/Home/components/HeroLines.jsx
@@ -2,60 +2,66 @@ import React from "react";
 
 const HeroLines = () => {
   return (
-    <div></div>
-    // <div>
-    //   <svg
-    //     id="heroLine1"
-    //     xmlns="http://www.w3.org/2000/svg"
-    //     className="absolute hidden lg:block opacity-0 flipped heroLine w-[34.84375vw] leftRightFixer1 bottom-[7.33906249999vw]"
-    //     viewBox="0 0 675.665 74.816"
-    //   >
-    //     <path
-    //       id="Path_101511"
-    //       data-name="Path 101511"
-    //       d="M-3806,711.985s281.7-67.628,669.043-68.194"
-    //       transform="translate(3809.617 -640.787)"
-    //       fill="none"
-    //       stroke="#8450ff"
-    //       strokeLinecap="round"
-    //       strokeWidth={6}
-    //     />
-    //   </svg>
-    //   <svg
-    //     id="heroLine2"
-    //     xmlns="http://www.w3.org/2000/svg"
-    //     className="absolute hidden lg:block opacity-0 flipped heroLine w-[10.15625vw] bottom-[9.871875vw]"
-    //     viewBox="0 0 201.364 35.191"
-    //   >
-    //     <path
-    //       id="Path_101512"
-    //       data-name="Path 101512"
-    //       d="M-2690.248,663.356c72.318-2.322,136.629-12.789,194.592-28.419"
-    //       transform="translate(2693.343 -631.26)"
-    //       fill="none"
-    //       stroke="#8450ff"
-    //       strokeLinecap="round"
-    //       strokeWidth={6}
-    //     />
-    //   </svg>
-    //   <svg
-    //     id="heroLine3"
-    //     xmlns="http://www.w3.org/2000/svg"
-    //     className="absolute hidden lg:block opacity-0 flipped heroLine leftRightFixer2 w-[7.55208333333vw] bottom-[23.3265625vw]"
-    //     viewBox="0 0 152.08 30.043"
-    //   >
-    //     <path
-    //       id="Path_101510"
-    //       data-name="Path 101510"
-    //       d="M-2039.476,405.022c47.375-19.92,94.079-29.007,144.63-19.086"
-    //       transform="translate(2043.404 -378.907)"
-    //       fill="none"
-    //       stroke="#8450ff"
-    //       strokeLinecap="round"
-    //       strokeWidth={6}
-    //     />
-    //   </svg>
-    // </div>
+    <div>
+      <svg 
+        id="heroLine1"
+        className="absolute hidden lg:block opacity-0 flipped heroLine w-[34.84375vw] leftRightFixer1 bottom-[7.33906249999vw]"
+      xmlns="http://www.w3.org/2000/svg" width="1925.035" height="1066.303" viewBox="0 0 1925.035 1066.303">
+        <path id="Path_104001" data-name="Path 104001" d="M913.05,934.915c-29.955,86.087,9.7,679.258-90,705.582-42.087,11.112-65.9-6.02-65.642-18.778s40-7.545,40-7.545-21.625-32.721-3.546-39.163c6.883-2.524,20.582-1.311,37.5,26.053,38.734,60.111-42.051,270.8-357.216,291.666-346.336,22.93-1283.018-212.1-1480.827,103.186" transform="translate(1009.439 -932.369)" fill="none" stroke="#8450ff" stroke-linecap="round" stroke-width="4"/>
+      </svg>
+
+      {/* <svg
+        xmlns="http://www.w3.org/2000/svg"
+        id="heroLine1"
+        className="absolute hidden lg:block opacity-0 flipped heroLine w-[34.84375vw] leftRightFixer1 bottom-[7.33906249999vw]"
+        viewBox="0 0 675.665 74.816"
+      >
+        <path
+          id="Path_101511"
+          data-name="Path 101511"
+          d="M-3806,711.985s281.7-67.628,669.043-68.194"
+          transform="translate(3809.617 -640.787)"
+          fill="none"
+          stroke="#8450ff"
+          strokeLinecap="round"
+          strokeWidth={6}
+        />
+      </svg> */}
+      {/* <svg
+        id="heroLine2"
+        xmlns="http://www.w3.org/2000/svg"
+        className="absolute hidden lg:block opacity-0 flipped heroLine w-[10.15625vw] bottom-[9.871875vw]"
+        viewBox="0 0 201.364 35.191"
+      >
+        <path
+          id="Path_101512"
+          data-name="Path 101512"
+          d="M-2690.248,663.356c72.318-2.322,136.629-12.789,194.592-28.419"
+          transform="translate(2693.343 -631.26)"
+          fill="none"
+          stroke="#8450ff"
+          strokeLinecap="round"
+          strokeWidth={6}
+        />
+      </svg> */}
+      {/* <svg
+        id="heroLine3"
+        xmlns="http://www.w3.org/2000/svg"
+        className="absolute hidden lg:block opacity-0 flipped heroLine leftRightFixer2 w-[7.55208333333vw] bottom-[23.3265625vw]"
+        viewBox="0 0 152.08 30.043"
+      >
+        <path
+          id="Path_101510"
+          data-name="Path 101510"
+          d="M-2039.476,405.022c47.375-19.92,94.079-29.007,144.63-19.086"
+          transform="translate(2043.404 -378.907)"
+          fill="none"
+          stroke="#8450ff"
+          strokeLinecap="round"
+          strokeWidth={6}
+        />
+      </svg> */}
+    </div>
   );
 };
 
