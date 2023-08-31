8899bb528f1720b625c02263f06ea5a7b7902d06
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2023-08-31 12:27:25 +0500

    origin

--- src/components/Pages/Home/components/CompanyValue.jsx
+++ src/components/Pages/Home/components/CompanyValue.jsx
@@ -6,7 +6,7 @@ const CompanyValue = ({ index, companyValue }) => {
       data-aos-anchor="anchorFOrCards"
       data-aos="fade"
       data-aos-delay={(index + 1) * 200}
-      className=" sm:pt-[6.09756097561vw] sm:w-[100%]  lg:pb-[2.34375vw] companyValues-start w-full lg:pt-[1.74479166667vw] pb-[24px] pt-[8.20512820513vw]   lg:w-[23.3333333333vw] bg-[#EBF0F7] lg:bg-[#F5EBE8] lg:rounded-[1.04166666667vw] rounded-[3.84615384615vw] flex  "
+      className=" sm:pt-[6.09756097561vw] sm:w-[100%]  lg:pb-[2.34375vw] companyValues-start w-full lg:pt-[1.74479166667vw] pb-[24px] pt-[8.20512820513vw]   lg:w-[23.3333333333vw] bg- [#EBF0F7] lg: bg-[#F5EBE8] lg:rounded-[1.04166666667vw] rounded-[3.84615384615vw] flex  "
     >
       <div className=" sm:h-full sm:w-[90%] sm:gap-y-[1.46341463415vw] w-[76.6666666667vw] gap-y-[4vw] lg:w-[19.1666666667vw] h-full   mx-[auto] flex flex-col lg:gap-y-[0.41666666666vw]">
         <h4 className="lg:h-[4.22708333333vw] sm:text-[2.68292682927vw] sm:leading-[3.90243902439vw] text-[4.871794871794872vw] lg:text-[1.354166666666667vw] lg:leading-[1.875vw] PingAR-Bold text-[#845FFF]  ">
