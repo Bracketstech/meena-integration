fd60436c5259c1681e7c635d802296062f9f812c
Ahmed <osman.ahmeda91@gmail.com>
2023-08-31 15:35:57 +0300

    changes

--- src/lib/client.js
+++ src/lib/client.js
@@ -5,7 +5,6 @@ export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      
       uri: "https://backend.meena-health.com/graphql",
     }),
   });
