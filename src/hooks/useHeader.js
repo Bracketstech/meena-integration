53703f9e47ec1de340c6a8cdcb89b962323e505c
Ahmed <osman.ahmeda91@gmail.com>
2023-09-10 12:56:41 +0300

    add tracking gtag

--- src/hooks/useHeader.js
+++ src/hooks/useHeader.js
@@ -4,6 +4,17 @@ const useHeader = (title, home) => {
   useEffect(() => {
     if (home) {
       document.querySelector("head").innerHTML += `
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?
+            id=G-2KMQN195TE"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+          gtag("config", "G-2KMQN195TE");
+        </script>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
